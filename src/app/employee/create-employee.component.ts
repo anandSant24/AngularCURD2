f44b4d79264903879169af60054811be1b2df730
AnandSant24 <anandsant24@gmail.com>
2020-02-02 21:04:49 -0800

    #7 setValue and patchValue methods

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -30,4 +30,25 @@ export class CreateEmployeeComponent implements OnInit {
     console.log("is it pristine",this.employeeForm.controls.fullName.pristine)
     console.log("is email filed touched",this.employeeForm.get('email').touched);  
   }
+
+  onLoadData():void{
+    this.employeeForm.setValue({
+      fullName: 'A',
+      email: 'a@a.com',
+      skills: {
+        skillName: 'JavaScript',
+        experienceInYears: '6',
+        proficiency: 'Beginner'
+      }
+    })
+  }
+
+  loadOnlyPartialData():void{
+    this.employeeForm.patchValue({
+      skills: {
+        skillName: 'JavaScript',
+        experienceInYears: '6'
+      }
+    })
+  }
 }
